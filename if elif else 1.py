9b9b40c80fe22419f06f602371048057a3415d8f
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 13:53:12 +0700

    file if elif else 1 selesai

---
+++ if elif else 1.py
@@ -0,0 +1,9 @@
+#variabel input kata dari user
+word = input("keyword : ")
+
+if word == "Spathiphyllum" :
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif word == "spathiphyllum" :
+    print("No, I want a big Spathiphyllum!")
+else :
+    print("Spathiphyllum! Not",word,"!")
