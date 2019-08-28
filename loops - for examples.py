7de56ea5dad091a3ad3d5efc0982d9d9998aa00c
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 16:27:16 +0700

    loops - for examples selesai

--- loops - for examples.py
+++ loops - for examples.py
@@ -1,1 +1,24 @@
 #3.1.2.5 Loops in Python | for
+
+# for range 1 parameter -> jumlah perulangan
+
+for i in range(10) :
+    print("perulangan ke",i)
+
+print()
+
+# for range 2 parameter -> angka awal perulangan, angka akhir perulangan
+
+a = 1
+for i in range(3,10) :
+    print(i," = perulangan ke",a)
+    a+=1
+
+print()
+
+# for range 3 parameter -> angka awal, angka akhir, pertambahan / iterasi
+
+for i in range(3,20,4) :
+    print(i)
+
+print()
