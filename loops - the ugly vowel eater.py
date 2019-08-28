1c457c26d85ed5fc0215bac55d6fd197ba34463e
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 17:53:56 +0700

    loops - the Ugly Vowel Eater selesai

--- loops - the ugly vowel eater.py
+++ loops - the ugly vowel eater.py
@@ -1,1 +1,9 @@
 # 3.1.2.10 LAB: The continue statement - the Ugly Vowel Eater
+
+userWord = input("Enter the word : ")
+userWord = userWord.upper()
+
+for letter in userWord : 
+    if (letter == "A" or letter == "I" or letter == "U" or letter == "E" or letter == "O"):
+        continue
+    print(letter)
