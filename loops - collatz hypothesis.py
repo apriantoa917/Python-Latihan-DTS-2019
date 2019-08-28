63489c34e5a91e534a735d64936246e16171351d
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 18:33:55 +0700

    loops - collatz hypothesis selesai

--- loops - collatz hypothesis.py
+++ loops - collatz hypothesis.py
@@ -1,1 +1,19 @@
 # 3.1.2.15 LAB: Collatz's hypothesis
+
+c0 = int(input("Enter the number : "))
+
+while c0 <= 0 :
+    print("must positive and greater than 0")
+    c0 = int(input("Enter the number : "))
+
+step = 0
+
+while c0 != 1:
+    step +=1
+    print(c0)
+    if c0 % 2 == 0:
+        c0 //= 2
+    else :
+        c0 = 3 * c0 + 1
+print(1)
+print("step = ",step)
