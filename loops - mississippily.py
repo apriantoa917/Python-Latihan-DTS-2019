186c7e2e68c5f02d130a5b574261cb66518aaf55
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 16:32:02 +0700

    loops - mississippily selesai

---
+++ loops - mississippily.py
@@ -0,0 +1,9 @@
+#3.1.2.6 LAB: Essentials of the for loop - counting mississippily
+
+import time
+
+for i in range(5) :
+    print(i+1,"Mississippi")
+    time.sleep(1)
+
+print("Ready or not, here I come!")
