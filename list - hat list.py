5af3bb311b77223678a0bcfcb8432066fd8d4a02
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 21:35:34 +0700

    list - hat list selesai

--- list - hat list.py
+++ list - hat list.py
@@ -1,1 +1,13 @@
 # 3.1.4.6 LAB: The basics of lists
+
+hatList = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+indices = int(input("enter a new number : "))
+
+hatList[2] = indices
+
+del hatList[4]
+
+print(len(hatList))
+
+print(hatList)
