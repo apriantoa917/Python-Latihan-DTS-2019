96c5a919d127d893bd71379a1eb61f96fbdebffd
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 17:40:41 +0700

    loops - chupacabra selesai

--- loops - chupacabra.py
+++ loops - chupacabra.py
@@ -1,1 +1,8 @@
 # 3.1.2.9 LAB: The break statement - Stuck in a loop
+
+while True :
+    word = input("Enter the word : ")
+    if word == "chupacabra" :
+        print("You've successfully left the loop.")
+        break
+    
