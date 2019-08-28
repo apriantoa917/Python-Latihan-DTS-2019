852e42da36e6ee84fa3c974f730ccb07f6c126e9
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 17:35:23 +0700

    loops - largest number selesai

--- loops - largest number.py
+++ loops - largest number.py
@@ -1,1 +1,40 @@
 # 3.1.2.8 Loop control in Python | break and continue
+
+# menggunakan infinite loops dengan break
+
+largestNumber = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end program: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largestNumber:
+        largestNumber = number
+
+if counter != 0:
+    print("The largest number is", largestNumber)
+else:
+    print("You haven't entered any number.")
+
+# menggunakan while dengan continue
+
+largestNumber = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largestNumber:
+        largestNumber = number
+    number = int(input("Enter a number or type -1 to end program: "))
+
+if counter:
+    print("The largest number is", largestNumber)
+else:
+    print("You haven't entered any number.")
