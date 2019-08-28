e083e8edf44ef1895c539996eff7502ca867018e
apriantoa917 <apriantoa917@gmail.com>
2019-08-28 15:47:32 +0700

    decision tax selesai

---
+++ decision - tax.py
@@ -0,0 +1,12 @@
+income = float(input("Enter the annual income: "))
+
+if income <= 85528 :
+    tax = ((18/100) * income) - 556.2
+elif income >= 85528 :
+    tax = 14839.2 + ((32/100) * (income - 85528))
+
+if tax <= 0 :
+    tax = 0
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
