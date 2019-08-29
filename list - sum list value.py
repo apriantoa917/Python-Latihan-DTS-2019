ab68febf831b52eb7f3942c938a869f4d4ba9f08
apriantoa917 <apriantoa917@gmail.com>
2019-08-29 08:07:38 +0700

    rev 1 list - sum list value

--- list - sum list value.py
+++ list - sum list value.py
@@ -1,5 +1,6 @@
 # 3.1.4.10 Lists - collections of data | lists and loops
 
+#   CARA 1
 # menjumlahkan nilai dari semua isi list
 
 myList = [10, 1, 8, 3, 5]
@@ -7,5 +8,15 @@ total = 0
 
 for i in range(len(myList)):
     total += myList[i]
+
+print(total)
+
+#   CARA 2
+
+myList = [10, 1, 8, 3, 5]
+total = 0
+
+for i in myList:
+    total += i
 
 print(total)
