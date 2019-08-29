767d82d82df12d3f4eadf954143ee0821ce3ccfe
apriantoa917 <apriantoa917@gmail.com>
2019-08-29 09:28:34 +0700

    list - the beatles selesai

--- list - the beatles.py
+++ list - the beatles.py
@@ -1,1 +1,40 @@
 # 3.1.4.13 LAB: The basics of lists - the Beatles
+
+#step 1
+
+beatles = []
+
+print(beatles)
+
+# step 2
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+print("Step 2:", beatles)
+
+# step 3
+
+for i in range(2):
+    member = input("next members : ")
+    beatles.append(member)
+
+print("Step 3:", beatles)
+
+# step 4
+
+for i in range(2):
+    length = len(beatles)-1
+    del beatles [length]
+
+print("Step 4:", beatles)
+
+# step 5
+
+beatles.insert(0, "Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
