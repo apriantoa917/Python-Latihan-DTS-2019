5ea82474606544dac975078f72c6cb17e7369cd3
apriantoa917 <apriantoa917@gmail.com>
2019-08-29 07:47:05 +0700

    list - append empty list selesai

--- list - append empty list.py
+++ list - append empty list.py
@@ -1,1 +1,14 @@
 # 3.1.4.9 Lists - collections of data | list methods
+
+# membuat list kosong 
+
+a = []
+
+print("isi list sebelum : ",a)
+
+# mengisi list dengan for
+
+for i in range(10):
+    a.append(i+1)
+
+print("isi list sesudah : ",a)
