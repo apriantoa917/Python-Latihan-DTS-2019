6f8602ba29c0962b2516ea2fc6bcb0f26a2bb049
apriantoa917 <apriantoa917@gmail.com>
2019-08-29 07:43:22 +0700

    list - append insert examples selesai

--- list - append insert examples.py
+++ list - append insert examples.py
@@ -1,1 +1,25 @@
 # 3.1.4.8 Lists - collections of data | list methods
+
+# append = menambahkan item kedalam list setelah elemen terakhir
+
+print("\nlist original")
+
+a = [1,2,3,4]
+
+print(a, "panjang list : ",len(a))
+
+# append list a dengan number 5
+
+a.append(5)
+
+print("\nlist yang di append")
+
+print(a, "panjang list : ",len(a))
+
+# insert = menambahkan item ke dalam list dengan posisi tertentu
+
+a.insert(0,0)
+
+print("\nlist yang di insert")
+
+print(a, "panjang list : ",len(a))
