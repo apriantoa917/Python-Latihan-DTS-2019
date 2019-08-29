f0465b43fa5059bc52fa58e79e53ae6dcb2b95e0
apriantoa917 <apriantoa917@gmail.com>
2019-08-29 09:10:30 +0700

    list - swapping examples selesai

--- list - swap examples.py
+++ list - swap examples.py
@@ -1,1 +1,28 @@
 # 3.1.4.11 Lists - collections of data | lists and loops
+
+# general swapping :
+
+var1 = 1
+var2 = 2
+temp = 0
+
+print("sebelum swap : \nvar 1 : ",var1," | var 2 :",var2)
+
+temp = var1
+
+var1 = var2
+
+var2 = temp
+
+print("\nsetelah swap : \nvar 1 : ",var1," | var 2 :",var2)
+
+# swappimh python
+
+var1 = 3
+var2 = 4
+
+print("\nsebelum swap : \nvar 1 : ",var1," | var 2 :",var2)
+
+var1, var2 = var2, var1
+
+print("\nsetelah swap : \nvar 1 : ",var1," | var 2 :",var2)
