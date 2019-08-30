7194b7516a245e4b362eba71ae47033a69c8d2d7
apriantoa917 <apriantoa917@gmail.com>
2019-08-30 13:52:07 +0700

    rev 1 list - slice

--- list - slice.py
+++ list - slice.py
@@ -27,3 +27,28 @@ print("part list : ", part_list)
 part_list = old_list[0:2]   # copy elemen index 0-1
 
 print("part list : ", part_list)
+
+
+# tambahan
+
+# copy hingga bagian tertentu
+
+new_list = old_list[:3] # copy dari awal (index 0) hingga 2
+
+print("new list : ", new_list)
+
+# copy mulai bagian tertentu
+
+new_list = old_list[2:] # copy mulai dari index 2 hingga akhir index
+
+print("new list : ", new_list)
+
+# copy bagian tertentu hingga beberapa index akhir
+
+new_list = old_list[2:-1] # copy nilai dari index 2 hingga 1 index dari index akhir (index akhir = 3, 3-1 = 2)
+
+print("new list : ", new_list)
+
+new_list = old_list[-3:-1] # copy nilai dari 3 index dari akhir index hingga 1 index dari index akhir 
+
+print("new list : ", new_list)
