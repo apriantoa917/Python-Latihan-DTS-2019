96db8f38dbc0168fa91c76de2cc7afe60b0d712f
apriantoa917 <apriantoa917@gmail.com>
2019-08-30 14:21:06 +0700

    list - in, not in selesai

---
+++ list - in, not in.py
@@ -0,0 +1,14 @@
+# 3.1.6.6 Operations on lists | in, not in
+
+# menggembalikan nilai boolean (True / False)
+
+myList = [0, 3, 12, 8, 2]
+
+# tes jawaban benar
+print( 0 in myList)
+print( 10 not in myList)
+
+# tes jawaban salah
+
+print( 100 in myList)
+print( 8 not in myList)
