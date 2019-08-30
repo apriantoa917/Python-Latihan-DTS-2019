dbfdf776dcd2cb62b84508a6b5cef41bb564509e
apriantoa917 <apriantoa917@gmail.com>
2019-08-30 14:15:01 +0700

    list - del selesai

--- list - del.py
+++ list - del.py
@@ -1,1 +1,26 @@
 # 3.1.6.5 Operations on lists | slices, del
+
+a = [0,1,2,3,4,5]
+
+# delete semua elemen list
+
+del a[:] # cara 1
+
+print(a)
+
+a = [0,1,2,3,4,5]
+
+# cara 2
+
+# del a -------------> jika ini aktif print a akan eror, list jadi hilang
+
+# print(a)
+
+# delete sebagian / bagian tertentu
+
+a = [0,1,2,3,4,5]
+
+del a[0:3] # hapus elemen index 0,1,2
+
+print(a)
+
