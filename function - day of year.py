921b4d7407d9d3e4ba9662571ed293d9144915f3
apriantoa917 <apriantoa917@gmail.com>
2019-09-09 14:27:58 +0700

    rev 1 function - day of year

--- function - day of year.py
+++ function - day of year.py
@@ -13,16 +13,14 @@ def isYearLeap(year):
 def daysInMonth(year, month):
     if year < 1582 or month < 1 or month > 12:
         return None
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    res = days[month - 1]
-    if month == 2 and isYearLeap(year):
-        res = 28 # <-- should be 28, prev in skeleton written 29
-    return res
+    else:
+        days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if isYearLeap(year):
+            days[1] = 28
+        return days[month-1]
 
-def dayOfYear(year, month, day):
-    days = 0
-    for m in range (1,month):
-        days+= daysInMonth(year,m)
-    return days
+day = 0
+for i in range(1,13):
+    day += daysInMonth(2002,i)
 
-print(dayOfYear(2000, 12, 31))
+print("day of year", day)
