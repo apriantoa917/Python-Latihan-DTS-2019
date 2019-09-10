f8b1488f817ad764970835950687e9e67f5010b5
apriantoa917 <apriantoa917@gmail.com>
2019-09-10 18:33:20 +0700

    function 2p - body mass index selesai

---
+++ function 2p - body mass index.py
@@ -0,0 +1,6 @@
+# 4.1.5.1 Creating functions | two-parameter functions - 4.1.5.2 Creating functions | two-parameter functions
+
+def bmi(weight, height):
+    return weight / height ** 2
+
+print(bmi(52.5, 1.65))
