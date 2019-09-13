7c636291d3e3b0c632a8a7037dfdbc3069d725e0
apriantoa917 <apriantoa917@gmail.com>
2019-09-13 20:46:57 +0700

    tuples list - conversion selesai

---
+++ tuples list - conversion.py
@@ -0,0 +1,13 @@
+# 4.1.6.10 SECTION SUMMARY (1/3)
+
+tuples = (1,2,3)
+
+print("tuple :",tuples)
+
+lists = list(tuples) # tuple ke list
+
+print("list :",lists)
+
+newTuples = tuple(lists) # list ke tuple
+
+print("tuple :",tuples)
