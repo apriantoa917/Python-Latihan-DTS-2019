2330cbfdc32c9e099c9d45b14ccf83f7e03c9d1f
apriantoa917 <apriantoa917@gmail.com>
2019-09-14 13:18:47 +0700

    project - tic-tac-toe selesai

---
+++ project - tic-tac-toe sample answer.py
@@ -0,0 +1,93 @@
+# 4.1.6.13 PROJECT: Tic-Tac-Toe
+from random import randrange
+
+def DisplayBoard(board):
+	print("+-------" * 3,"+",sep="")
+	for row in range(3):
+		print("|       " * 3,"|",sep="")
+		for col in range(3):
+			print("|   " + str(board[row][col]) + "   ",end="")
+		print("|")
+		print("|       " * 3,"|",sep="")
+		print("+-------" * 3,"+",sep="")
+
+def EnterMove(board):
+	ok = False	# fake assumption - we need it to enter the loop
+	while not ok:
+		move = input("Enter your move: ") 
+		ok = len(move) == 1 and move >= '1' and move <= '9' # is user's input valid?
+		if not ok:
+			print("Bad move - repeat your input!") # no, it isn't - do the input again
+			continue
+		move = int(move) - 1 	# cell's number from 0 to 8
+		row = move // 3 	# cell's row
+		col = move % 3		# cell's column
+		sign = board[row][col]	# check the selected square
+		ok = sign not in ['O','X'] 
+		if not ok:	# it's occupied - to the input again
+			print("Field already occupied - repeat your input!")
+			continue
+	board[row][col] = 'O' 	# set '0' at the selected square
+
+def MakeListOfFreeFields(board):
+	free = []	# the list is empty initially
+	for row in range(3): # iterate through rows
+		for col in range(3): # iterate through columns
+			if board[row][col] not in ['O','X']: # is the cell free?
+				free.append((row,col)) # yes, it is - append new tuple to the list
+	return free
+
+
+def VictoryFor(board,sgn):
+	if sgn == "X":	# are we looking for X?
+		who = 'me'	# yes - it's computer's side
+	elif sgn == "O": # ... or for O?
+		who = 'you'	# yes - it's our side
+	else:
+		who = None	# we should not fall here!
+	cross1 = cross2 = True  # for diagonals
+	for rc in range(3):
+		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
+			return who
+		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
+			return who
+		if board[rc][rc] != sgn: # check 1st diagonal
+			cross1 = False
+		if board[2 - rc][2 - rc] != sgn: # check 2nd diagonal
+			cross2 = False
+	if cross1 or cross2:
+		return who
+	return None
+
+def DrawMove(board):
+	free = MakeListOfFreeFields(board) # make a list of free fields
+	cnt = len(free)
+	if cnt > 0:	# if the list is not empty, choose a place for 'X' and set it
+		this = randrange(cnt)
+		row, col = free[this]
+		board[row][col] = 'X'
+
+board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # make an empty board
+board[1][1] = 'X' # set first 'X' in the middle
+free = MakeListOfFreeFields(board)
+humanturn = True # which turn is it now?
+while len(free):
+	DisplayBoard(board)
+	if humanturn:
+		EnterMove(board)
+		victor = VictoryFor(board,'O')
+	else:	
+		DrawMove(board)
+		victor = VictoryFor(board,'X')
+	if victor != None:
+		break
+	humanturn = not humanturn		
+	free = MakeListOfFreeFields(board)
+
+DisplayBoard(board)
+if victor == 'you':
+	print("You won!")
+elif victor == 'me':
+	print("I won")
+else:
+	print("Tie!")
---
+++ project - tic-tac-toe.py
@@ -0,0 +1,94 @@
+# 4.1.6.13 PROJECT: Tic-Tac-Toe
+from random import randrange
+import time
+
+block = [None,None,None,None,"X",None,None,None,None,]
+
+result = "Draw"
+
+
+
+def showBoard():
+    print('+-------+-------+-------+')
+    print('|       |       |       |')
+    print('|  ', boardValue(1), '  |  ', boardValue(2), '  |  ', boardValue(3), '  |')
+    print('|       |       |       |')
+    print('+-------+-------+-------+')
+    print('|       |       |       |')
+    print('|  ', boardValue(4), '  |  ', boardValue(5),'  |  ', boardValue(6), '  |')
+    print('|       |       |       |')
+    print('+-------+-------+-------+')
+    print('|       |       |       |')
+    print('|  ', boardValue(7), '  |  ', boardValue(8), '  |  ', boardValue(9), '  |')
+    print('|       |       |       |')
+    print('+-------+-------+-------+')
+
+def boardValue(num):
+    if (block[num-1] == None):
+        return num
+    return block[num-1]
+
+def userInput():
+    print()
+    print('Turn : Hooman')
+    user = int(input('choose number :'))
+    if (block[user-1] == None):
+        block[user-1] = "O"
+        showBoard()
+    else:
+        print('kotak yang dipilih sudah terisi')
+        userInput()
+
+def compInput():
+    comp = randrange(1,10)
+    if (block[comp-1] == None):
+        block[comp-1] = "X"
+        print()
+        print('Turn : Computer')
+        print('choose number :',comp)
+        showBoard()
+    else:
+        compInput()
+
+def score():
+    global result
+    if(block[0] == "O" and block[1] == "O" and block[2] == "O" or\
+        block[0] == "O" and block[3] == "O" and block[6] == "O" or\
+            block[2] == "O" and block[5] == "O" and block[8] == "O" or\
+                block[6] == "O" and block[7] == "O" and block[8] == "O"):
+        result = "Hooman"
+    elif (block[0] == "X" and block[1] == "X" and block[2] == "X" or\
+        block[3] == "X" and block[4] == "X" and block[5] == "X" or\
+            block[6] == "X" and block[7] == "X" and block[8] == "X" or\
+                block[0] == "X" and block[3] == "X" and block[6] == "X" or\
+                    block[1] == "X" and block[4] == "X" and block[7] == "X" or\
+                        block[2] == "X" and block[5] == "X" and block[8] == "X" or\
+                            block[0] == "X" and block[4] == "X" and block[8] == "X" or\
+                                block[2] == "X" and block[4] == "X" and block[6] == "X"):
+        result = "Computer"
+    else:
+        result = "Draw"
+
+# computer first
+print()
+print('Turn : Computer')
+print('choose number :',5)
+showBoard()
+
+counter = 1
+while counter <= 9 and result == "Draw":
+    if counter % 2 != 0:
+        #computer turn
+        if counter != 1:
+            time.sleep(1)
+            compInput()
+    else:
+        # hooman turn
+        userInput()
+    score()
+    counter += 1
+
+print()
+print('-----WIN :',result,'-----')
+print()
+
