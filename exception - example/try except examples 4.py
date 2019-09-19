aa21b33b0f6b8007b77067465c1b26d58ffb57f1
apriantoa917 <apriantoa917@gmail.com>
2019-09-19 10:12:41 +0700

    rev - penambahan beberapa komentar

--- exception - example/try except examples 4.py
+++ exception - example/try except examples 4.py
@@ -18,4 +18,26 @@ try:
 except ArithmeticError:
     print('ooooppps')
 
-# keduanya bisa menjalankan kode yang ada di dalam exceptions karena kelas ZeroDivisionError berada dalam ArithmeticError
+# keduanya (percobaan 1&2) bisa menjalankan kode yang ada di dalam exceptions karena kelas ZeroDivisionError berada dalam ArithmeticError
+
+try:
+    y = 1 / 0
+except ZeroDivisionError:
+    print("Zero Division!")
+except ArithmeticError:
+    print("Arithmetic problem!")
+
+print("THE END.")
+
+
+# kode dibawah eror karena order / urutan aritmetic merupakan class general dari zerodivision (terdeteksi kelas aritmetik lebih luas harus diletakkan setelah klass spesifik (dibawah except zero division))
+# try:
+#     y = 1 / 0
+# except ArithmeticError:
+#     print("Arithmetic problem!")
+# except ZeroDivisionError:
+#     print("Zero Division!")
+
+# print("THE END.")
+
+# ketika kedua kondisi memenuhi untuk masuk ke except, urutan menentukan. program akan menjalankan kode exception terdekat 
