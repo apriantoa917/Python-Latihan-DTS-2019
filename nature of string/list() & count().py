315b4b220ba53b5241b64940f97560f3d4626601
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:45:42 +0700

    menambah list() & count()

---
+++ nature of string/list() & count().py
@@ -0,0 +1,12 @@
+# 5.1.8.14 The nature of strings in Python
+
+# Demonstrating the list() function
+print(list("abcabc"))
+
+# list : mengkonversi sebuah string menjadi setiap elemen di dalam list
+
+# Demonstrating the count() method
+print("abcabc".count("b"))
+print('abcabc'.count("d"))
+
+# count : menghitung jumlah sebuah karakter yang sama dalam suatu kata
