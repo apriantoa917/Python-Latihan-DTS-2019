6b126c2f46d197048e2b6aa65e600a8dfed5dd3e
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:42:30 +0700

    menambah file index().py & menambah tag bab di min() & max().py

---
+++ nature of string/index().py
@@ -0,0 +1,8 @@
+# 5.1.8.13 The nature of strings in Python
+
+# Demonstrating the index() method
+print("aAbByYzZaA".index("b"))
+print("aAbByYzZaA".index("Z"))
+print("aAbByYzZaA".index("A"))
+
+# index() : mengembalikan index dimana karakter itu berada. index yang diambil adalah index terdekat / pertama yang ditemukan
--- nature of string/min() & max().py
+++ nature of string/min() & max().py
@@ -1,3 +1,5 @@
+# 5.1.8.11 The nature of strings in Python- 5.1.8.12 The nature of strings in Python
+
 # Demonstrating min() - Example 1
 print(min("aAbByYzZ"))
 print(max("aAbByYzZ"))
