b1cfaccc81a96d741f6cb5674e023a131ef9ad10
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:08:11 +0700

    membuat file chr()

---
+++ nature of string/chr().py
@@ -0,0 +1,13 @@
+# 5.1.8.5 The nature of strings in Python
+
+# chr() = character = fungsi untuk mengembalikan sebuah karakter dari suatu ASCII / unicode
+
+# Demonstrating the chr() function
+
+print(chr(97))
+print(chr(945)) #alpha
+
+x = 'a'
+
+# pembuktian
+print(chr(ord(x)) == x)
