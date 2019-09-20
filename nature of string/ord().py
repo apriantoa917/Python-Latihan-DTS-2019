068bb40e4003eac7adf3b380f1722ca7fbf65c1b
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 08:53:26 +0700

    menambah file ord()

---
+++ nature of string/ord().py
@@ -0,0 +1,13 @@
+# 5.1.8.4 The nature of strings in Python
+
+# ord() = ordinal = fungsi untuk mengembalikan nilai ASCII / unicode dari sebuah karakter
+
+# Demonstrating the ord() function
+
+ch1 = 'a' 
+ch2 = ' ' # space
+
+print(ord(ch1))
+print(ord(ch2))
+# print(ord('String')) argumen eror karena yang dibutuhkan hanya 1 karakter
+print(ord('ę'))
