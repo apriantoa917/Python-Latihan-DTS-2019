6c347b995ce42f9ad79088f3cfd37b9bf2828ec2
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:38:51 +0700

    menambah  file min() & max().py

---
+++ nature of string/min() & max().py
@@ -0,0 +1,15 @@
+# Demonstrating min() - Example 1
+print(min("aAbByYzZ"))
+print(max("aAbByYzZ"))
+
+
+# Demonstrating min() - Examples 2 & 3
+t = 'The Knights Who Say "Ni!"'
+print('[' + min(t) + ']')
+print('[' + max(t) + ']')
+
+t = [0, 1, 2]
+print(min(t))
+print(max(t))
+
+# min dan max melihat urutan berdasarkan nomer urutan ASCII
