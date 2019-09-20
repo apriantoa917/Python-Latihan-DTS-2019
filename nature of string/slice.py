c2d2542a4f8c5bfc4f6da98547dd4825d2d2a069
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:25:53 +0700

    menambah file slice.py

---
+++ nature of string/slice.py
@@ -0,0 +1,13 @@
+# 5.1.8.7 The nature of strings in Python
+
+# Slices
+
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2]) 
