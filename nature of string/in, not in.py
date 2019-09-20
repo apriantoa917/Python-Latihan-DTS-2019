c9104322c5745fe1816e59994e7381d668c0364d
apriantoa917 <apriantoa917@gmail.com>
2019-09-20 09:27:26 +0700

    menambah file in, not in.py

---
+++ nature of string/in, not in.py
@@ -0,0 +1,9 @@
+# 5.1.8.8 The nature of strings in Python
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" in alphabet)
+print("F" in alphabet)
+print("1" in alphabet)
+print("ghi" in alphabet)
+print("Xyz" in alphabet)
