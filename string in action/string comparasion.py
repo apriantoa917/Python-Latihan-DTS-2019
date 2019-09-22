703fa654f12a5af7b5f709e4769cb1ae1fbde1c9
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 19:55:18 +0700

    membuat folder string in action dan  menambah dfile string compatation.py

---
+++ string in action/string comparasion.py
@@ -0,0 +1,17 @@
+# 5.1.10.1 String in action, 5.1.10.2 String in action
+
+
+# operator yang bisa digunakan ==,!=,>,>=,<,<=
+
+print('alpha' == 'alpha')
+print('alpha' == 'Alpha') #case sensitive
+print('alpha' != 'Alpha') 
+print('alpha' > 'alpha')
+print('alpha' >= 'alpha')
+print('alpha' < 'alphabet')
+print('alphabet' < 'alpha')
+'10' == 10
+'10' != 10
+'10' == 1
+'10' != 1
+# '10' > 10 eror karena >,>=,<,<= tidak mendukung komparasi int dan str
