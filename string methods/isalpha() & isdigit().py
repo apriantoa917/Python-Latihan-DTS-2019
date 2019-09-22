89bc0ed833b32aeace03ea7dc05e0e5a85c9d4a3
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 11:57:48 +0700

    menambah file isalpha() & isdigit().py

---
+++ string methods/isalpha() & isdigit().py
@@ -0,0 +1,9 @@
+# 5.1.9.6 String methods
+
+# Example 1: Demonstrating the isapha() method
+print("Moooo".isalpha())
+print('Mu40'.isalpha())
+
+# Example 2: Demonstrating the isdigit() method
+print('2018'.isdigit())
+print("Year2019".isdigit())
