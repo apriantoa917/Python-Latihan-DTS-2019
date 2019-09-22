b15d6a6fd854f894fa8eca74df08f32d3c07f904
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 12:08:54 +0700

    menambah file join().py

---
+++ string methods/join().py
@@ -0,0 +1,10 @@
+# 5.1.9.8 String methods
+
+# join()= menggabungkan beberapa elemen string menjadi 1 dengan separator tertentu
+# elemen string berupa list
+
+# Demonstrating the join() method
+print(",".join(["omicron", "pi", "rho"]))
+
+lst = ['kata pertama','kata kedua','kata ketiga','kata keempat']
+print(', lalu ini '.join(lst))
