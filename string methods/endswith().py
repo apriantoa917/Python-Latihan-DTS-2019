4dcde59f9c101da9edcefe712a48c21a2e233753
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 10:20:22 +0700

    menambah file endswith().py

---
+++ string methods/endswith().py
@@ -0,0 +1,22 @@
+# 5.1.9.3 String methods
+
+# endswith() = ends with = mengecek apakah akhiran dari suatu kata mengandung kata yang dimaksud dan mengembalikan nilai T/F
+
+kata = 'Hello'
+
+print(kata.endswith('a'))
+print(kata.endswith('o'))
+
+def check(kata,cari):
+    if kata.endswith(cari):
+        print(kata,'berakhiran dengan',cari)
+    else:
+        print(kata,'tidak berakhiran dengan',cari)
+
+check(kata,'elo')
+check(kata,'ello')
+check(kata,'o')
+check(kata,'e')
+check('aprianto','a')
+check('aprianto','o')
+
