28bb8bd611ce8c45e783434284e1c762f46c234b
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 13:52:53 +0700

    rename join() ke join() & split().py dan menambah beberapa materi

--- string methods/join() & split().py
+++ string methods/join() & split().py
@@ -8,3 +8,12 @@ print(",".join(["omicron", "pi", "rho"]))
 
 lst = ['kata pertama','kata kedua','kata ketiga','kata keempat']
 print(', lalu ini '.join(lst))
+
+# split() = menggabungkan sebuah kata / kalimat menjadi setiap elemen list
+
+kata = 'ini adalah sebuah kalimat yang tidak utuh, biasa dibilang frasa\nini adalah baris kedua'
+
+lst = kata.split()
+
+print(kata)
+print(lst)
