a5ee2b8704a03dbd49cfe30b1ef381430b8f418c
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 10:13:38 +0700

    menambah file center().py

---
+++ string methods/center().py
@@ -0,0 +1,6 @@
+# 5.1.9.2 String methods
+
+# Demonstrating the center() method
+print('[' + 'alpha'.center(10) + ']')
+print('[' + 'alpha'.center(10,'.') + ']')
+print('[' + ' gamma '.center(20, '*') + ']')
