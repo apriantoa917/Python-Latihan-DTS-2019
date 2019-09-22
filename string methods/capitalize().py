99998ceff4da018f4b95d81725b98eeb4d3572f2
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 10:11:00 +0700

    membuat folder string method dan file capitalize.py

---
+++ string methods/capitalize().py
@@ -0,0 +1,10 @@
+# 5.1.9.1 String methods
+
+#capitalize() = menjadikan sebuah kata menjadi kapital(hanya huruf depan kata pertama yang besar)
+
+print("Alpha".capitalize())
+print('ALPHA'.capitalize())
+print(' Alpha'.capitalize())
+print('123'.capitalize())
+print("αβγδ".capitalize())
+print("alpha beta teta".capitalize())
