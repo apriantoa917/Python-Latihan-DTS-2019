42e8628bdb4d6986640b3c53a78491ac1e894eda
apriantoa917 <apriantoa917@gmail.com>
2019-09-22 12:00:54 +0700

    menambah file islowe(), isspace() & issuper().py

---
+++ string methods/islower(), isspace() & issuper().py
@@ -0,0 +1,15 @@
+# 5.1.9.7 String methods
+
+# Example 1: Demonstrating the islower() method
+print("Moooo".islower())
+print('moooo'.islower())
+
+# Example 2: Demonstrating the isspace() method
+print(' \n '.isspace())
+print(" ".isspace())
+print("mooo mooo mooo".isspace())
+
+# Example 3: Demonstrating the isupper() method
+print("Moooo".isupper())
+print('moooo'.isupper())
+print('MOOOO'.isupper())
