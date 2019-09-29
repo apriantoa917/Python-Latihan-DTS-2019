7bce7f2367451c08b8d92012c9907fe0c896e668
apriantoa917 <apriantoa917@gmail.com>
2019-09-29 17:49:40 +0700

    menambah file examples 1

---
+++ OOP/OOP - Methods/examples 1.py
@@ -0,0 +1,43 @@
+# 6.1.4.1 OOP: Methods
+
+# There is one fundamental requirement - a method is obliged to have at least one parameter(there are no such thing as parameterless methods - a method may be invoked without an argument, but not declared without parameters).
+# self biasa digunakan untuk parameter, beberapa kondisi akan eror jika parameter pertama != self
+
+class Classy:
+    def method(self):
+        print("method")
+
+
+obj = Classy()
+obj.method()
+
+
+class Classy2:
+    def method(self, par):
+        print("method:", par)
+
+
+obj = Classy2()
+obj.method(1)
+obj.method(2)
+obj.method(3)
+
+class Classy3:
+    varia = 2
+    def method(self):
+        print(self.varia, self.var)
+
+obj = Classy3()
+obj.var = 3
+obj.method()
+
+class Classy4:
+    def other(self):
+        print("other")
+
+    def method(self):
+        print("method")
+        self.other()
+
+obj = Classy4()
+obj.method()
